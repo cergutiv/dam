1be9b2abc087755d699e31a8380776952a7847ad
cergutiv <58826991+cergutiv@users.noreply.github.com>
2019-12-12 21:15:25 +0100

    Add files via upload

---
+++ PracticaProgramacion/src/practicaprogramacion/Interfaz.java
@@ -0,0 +1,73 @@
+package practicaprogramacion;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/**
+ *
+ * @author cergutiv
+ */
+public class Interfaz {
+
+    public static void main(String[] args) {
+        /**/
+        Logica logica = new Logica();
+        Scanner sc = new Scanner(System.in);
+        String nickname;
+        int rappelMonedas = logica.rappelMonedas;
+        boolean continuar = true;
+        String proseguir = "";
+        /**/
+        /////////////////////////////////////////
+        System.out.println("¡¡Bienvenido!!");
+        System.out.println("Para comenzar indica tu nombre: (sin espacios, el primer caracter no puede ser un número)");
+        System.out.println("Los unicos valores especiales aceptados son: punto (.), coma(,), barra baja(_), arroba(@) y almohadilla(#)");
+        nickname = sc.nextLine();
+        logica.comprobarNick(nickname);
+        logica.jugador = nickname;
+        System.out.println("Bienvenido " + nickname);
+        while (continuar) {
+            System.out.println("Se ha generado una fecha aleatoria, " + nickname + " ¡a ver cuánto puedes tardar!");
+            LocalDate fechaAleatoria = logica.fechaRandom();
+            String fechaAleatoriaFormateada = logica.LocalDateFormateado(fechaAleatoria);
+            /*QUITAR AL ACABAR*/
+            System.out.println(fechaAleatoriaFormateada);
+            int diaFechaAleatoria = logica.diaFechas(fechaAleatoria);
+            int mesFechaAleatoria = logica.diaMes(fechaAleatoria);
+            /*QUITAR AL ACABAR*/
+            System.out.println(diaFechaAleatoria + " " + mesFechaAleatoria);
+            long tiempoInicio = Logica.startTiempo();
+            System.out.println("Introduce una fecha (formato dd-MM-yyyy)");
+            String fechaUsuarioString = sc.nextLine();
+            LocalDate fechaUsuarioLocalDate = Logica.localDateUsuario(fechaUsuarioString);
+            /*QUITAR AL ACABAR*/
+            System.out.println("Has introducido " + fechaUsuarioLocalDate);
+            int diaFechaUsuario = logica.diaFechas(fechaUsuarioLocalDate);
+            int mesFechaUsuario = logica.diaMes(fechaUsuarioLocalDate);
+            /*QUITAR AL ACABAR*/
+            System.out.println(diaFechaUsuario + " " + mesFechaUsuario);
+            
+            boolean checkAcierto = logica.acertarMesYDia(mesFechaUsuario, mesFechaAleatoria, diaFechaUsuario, diaFechaAleatoria);
+
+            if (!logica.acertarMesYDia(mesFechaUsuario, mesFechaAleatoria, diaFechaUsuario, diaFechaAleatoria)) {
+                do {
+                    fechaUsuarioString = sc.nextLine();
+                    fechaUsuarioLocalDate = Logica.localDateUsuario(fechaUsuarioString);
+                    diaFechaUsuario = logica.diaFechas(fechaUsuarioLocalDate);
+                    mesFechaUsuario = logica.diaMes(fechaUsuarioLocalDate);
+                    System.out.println(checkAcierto);
+                    System.out.println("Vuelve a intentarlo" + nickname);
+                } while (!logica.acertarMesYDia(mesFechaUsuario, mesFechaAleatoria, diaFechaUsuario, diaFechaAleatoria));
+            }
+            long tiempoFinal = Logica.finishTiempo();
+            long compararInicioConFinalMils = Logica.compararInicioConFin(tiempoInicio, tiempoFinal);
+            int minutos = Logica.toMinutes(compararInicioConFinalMils);
+            rappelMonedas = logica.sumarRappelMonedas(minutos);
+            System.out.println(rappelMonedas);
+            System.out.println("¿Quieres continuar? (S/N (Por defecto es N))");
+            proseguir = sc.nextLine();
+            continuar = Logica.comprobarFinal(proseguir);
+            System.out.println(continuar);
+        }
+    }
+}
---
+++ PracticaProgramacion/src/practicaprogramacion/Logica.java
@@ -0,0 +1,204 @@
+package practicaprogramacion;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author cergutiv
+ */
+public class Logica {
+
+    //variables y demas
+    static Scanner sc = new Scanner(System.in);
+    public int rappelMonedas = 0;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    Random r = new Random();
+    public String jugador; //para almacenar el nombre del jugador
+////////////////////////////////////////////////////////////
+
+    /*Intentar que sea void*/
+    public void comprobarNick(String input) {
+        char letter;
+        int i = 0;
+        boolean permitido = true;
+        //Comprobar si el nick está vacío y si el primer valor es un número
+        if (input.isEmpty() || Character.isDigit(input.charAt(0))) {
+            do {
+                System.out.println("Error, vuelve a introducirlo (no puede estar vacío \n o tener un número en la primera posición)");
+                input = sc.nextLine();
+            } while (input.isEmpty() || Character.isDigit(input.charAt(0)));
+        }
+
+
+        //Comprobar todos los valores para ver si son punto (.), coma(,), _, @ o #.
+/*cambiar*/ for (i = 0; i < input.length(); ++i) {
+            char inputChar = input.charAt(i);
+            String diccionario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz@#,._ÁÉÍÓÚáéíóú0123456789Üü";
+
+            if (diccionario.contains(String.valueOf(inputChar))) {
+                permitido = true;
+            } else {
+                System.out.println("Carácter no soportado");
+                permitido = false;
+            }
+
+        }
+    }
+    public static boolean comprobarFinal(String input){
+        boolean continuar;
+        switch (input.toLowerCase().charAt(0)){
+            case 's':
+                return continuar = true;
+            default: 
+                return continuar = false;
+        }
+        
+    } 
+    public static long startTiempo() {
+        long now = System.currentTimeMillis();
+        return now;
+    }
+
+    public static long finishTiempo() {
+        long fin = System.currentTimeMillis();
+        return fin;
+    }
+
+    public static long compararInicioConFin(long principio, long acabado) {
+        long tiempoPasadoMils = acabado - principio;
+        return tiempoPasadoMils;
+    }
+
+    public static int toMinutes(long tiempoPasadoMils) {
+        int minutos = (int) (tiempoPasadoMils / 60000);
+        return minutos;
+    }
+
+    public int sumarRappelMonedas(int minutos) {
+        int monedas = rappelMonedas;
+        if (minutos == 0) {
+            monedas = rappelMonedas + 100;
+        } else if (minutos <= 15 || minutos != 0) {
+            monedas = rappelMonedas + 50;
+        } else if (minutos <= 30 || minutos != 15 || minutos != 0) {
+            monedas = rappelMonedas + 25;
+        } else {
+            monedas = rappelMonedas + 5;
+        }
+        return monedas;
+    }
+
+    //Creacion de fecha aleatoria del mismo año de ejecucion
+    public LocalDate fechaRandom() {
+        //instancia actual
+        LocalDate hoy = LocalDate.now();
+        //año actual
+        int annoJuego = hoy.getYear();
+        //fecha minima
+        LocalDate fechaMin = LocalDate.of(annoJuego, Month.JANUARY, 1);
+        //fecha maxima
+        LocalDate fechaMax = LocalDate.of(annoJuego, Month.DECEMBER, 31);
+        //numero aleatorio entre 
+        int numMesRandom = r.nextInt(12);
+        if (numMesRandom == 0) {
+            while (numMesRandom == 0) {
+                numMesRandom = r.nextInt(12);
+            }
+        }
+        //mes aleatorio
+        LocalDate conversionAMes = LocalDate.of(annoJuego, numMesRandom, 1);
+        Month mesRandom = conversionAMes.getMonth();
+        //año bisiesto
+        boolean bisiesto = true;
+        if (LocalDate.now().lengthOfYear() == 366) {
+            bisiesto = true;
+        } else {
+            bisiesto = false;
+        }
+        //dia aleatorio
+        int maxDias = mesRandom.length(bisiesto);
+        int diaRandom = r.nextInt(maxDias);
+        if (diaRandom == 0) {
+            while (diaRandom == 0) {
+                diaRandom = r.nextInt(maxDias);
+            }
+        }
+        //Fecha aleatoria definitiva
+        LocalDate fechaRandom = LocalDate.of(annoJuego, mesRandom, diaRandom);
+        //return
+        return fechaRandom;
+    }
+
+    public String LocalDateFormateado(LocalDate localdate) {
+        String fechaFormateada = formatter.format(localdate);
+        return fechaFormateada;
+
+    }
+
+    public static LocalDate localDateUsuario(String userInput) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate date = LocalDate.parse(userInput, dateFormat);
+        return date;
+    }
+//obtener los dias y los meses de las fechas
+
+    public int diaFechas(LocalDate fechaAleatoria) {
+        int dia = fechaAleatoria.getDayOfMonth();
+        return dia;
+    }
+
+    public int diaMes(LocalDate fechaAleatoria) {
+        int mes = fechaAleatoria.getMonthValue();
+        return mes;
+    }
+
+    public boolean acertarMes(int mes1, int mes2) {
+        boolean b = true;
+        if (mes1 == mes2) {
+            b = true;
+        } else {
+            b = false;
+        }
+        return b;
+    }
+
+    public int diasEntreFechas(int v1, int v2) {
+        int resultado = 0;
+        if (v1 > v2) {
+            resultado = v1 - v2;
+        } else if (v2 > v1) {
+            resultado = v2 - v1;
+        } else if (v1 == v2) {
+            resultado = 0;
+        }
+        return resultado;
+
+    }
+
+    public boolean acertarMesYDia(int mes1, int mes2, int dia1, int dia2) {
+        boolean haAcertadoMeses = mes1 == mes2;
+        boolean haAcertadoDias = dia1 == dia2;
+        int diferenciaDias = Math.abs(dia1 - dia2);
+        if (!haAcertadoMeses) {
+            System.out.println("Pista: has fallado el mes");
+        }
+        if (haAcertadoDias && haAcertadoMeses) {
+            /*Mirar lo de las rappelmonedas*/
+            System.out.println("¡Enhorabuena! " + jugador + " ¡Has acertado la fecha!");
+            return true;
+        } else if (haAcertadoMeses && !haAcertadoDias) {
+            System.out.println("Has acertado el mes, pero no el día " + jugador);
+            if (diferenciaDias <= 15) {
+                System.out.println("El dia esta en un rango de entre 1 y 15 días respecto a tu fecha");
+            } else {
+                System.out.println("Sigue probando " + jugador);
+            }
+        }
+        return false;
+    }
+
+}
